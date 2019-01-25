29cd27aaf4ef3e481716aaabcd3ca259a69c2130
Mina Yen <mina102636@gmail.com>
2019-01-25 22:41:21 +0800

    Initial commit

---
+++ Assets/Core.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Core : MonoBehaviour
+{
+
+    //介面元件
+    public Text pname;
+    public Image portrait;
+    public Text price;
+
+    public void Change()
+    {
+        pname.text = "Caught";
+        price.text = "0";
+        portrait.sprite = Resources.Load<Sprite>("Person");
+    }
+}
